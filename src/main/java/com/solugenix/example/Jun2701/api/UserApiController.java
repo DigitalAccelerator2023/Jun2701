3813e3947383e87fd1219df6cc1268dcf5019fb5
AbhinandanMaheshwari <AbhinandanMaheshwari@BDS-1B4ZN93>
2023-06-27 13:43:18 +0530

    Initial commit

---
+++ src/main/java/com/solugenix/example/Jun2701/api/UserApiController.java
@@ -0,0 +1,62 @@
+package com.solugenix.example.Jun2701.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2023-06-27T13:43:03.453+05:30")
+
+@Controller
+public class UserApiController implements UserApi {
+
+    private static final Logger log = LoggerFactory.getLogger(UserApiController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public UserApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<List<>> getUsers() {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/xml")) {
+            try {
+                return new ResponseEntity<List<>>(objectMapper.readValue("null", List.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/xml", e);
+                return new ResponseEntity<List<>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<List<>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<List<>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<List<>>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
